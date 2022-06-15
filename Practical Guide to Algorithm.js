b6e70196c2e56abf737da42adf13b9a4544c1464
Jun Choi <48137126+cjh4549@users.noreply.github.com>
2022-06-15 17:12:38 -0400

    Spatial Complexity

--- Practical Guide to Algorithm.js
+++ Practical Guide to Algorithm.js
@@ -34,4 +34,27 @@ function getMiddleOfArray(array) {
 
 // Big O in real life
 // If you have a function to sort and filter comments, few comments wouldn't matter.
-// But if it's a website like Reddit with thousands of comments, writing a Big(n2) function would be a disaster. 
+// But if it's a website like Reddit with thousands of comments, writing a Big(n2) function would be a disaster.
+
+
+// Spatial Complexity
+// How much RAM and memory are you using? How much stuff are you creating? 
+
+// Linear
+// Let's say there are 10 items in an array, and for each item, you have to create 10 more arrays.
+// This can quickly churn up memory and you want to always avoid linear complexity.
+// Usually have to do with creating more arrays.
+// O(n)
+
+// Logarithmic
+// Every item you iterate, you are creating less items. Ex) 100 -> 12 more arrays, 10 -> 7 more arrays...
+// O(log n)
+
+// Constant
+// You create a same amount of new arrays (ex. 5) no matter how many items in an array (100 vs 10000). 
+// O(1)
+
+// Quadratic
+// What if we had an app that calculates a distance between two zip codes?
+
+// Spatial complexity can be ignored if memory is not much of an issue with algorithms.
